9c18ee301b4e466efdac6a8fc67c7e184a2efbcf
Jen Miao <jenmiao@gmail.com>
2020-02-15 21:14:41 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,7 +19,7 @@ end
 
 def remove_element_from_end_of_array(array)
   my_array = ['blue', 'green', 'black', 'arrays!']
-  my_array.pop
+  new_arr = my_array.pop
 end
 
 def remove_element_from_start_of_array(array)
