f0f6569f3008d0d197347f648ef7aa2f9bb72a13
Jen Miao <jenmiao@gmail.com>
2020-02-15 21:42:17 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,35 +1,48 @@
 def create_an_empty_array
-  
+  []
 end
 
 def create_an_array
-  
+  ['blue', 'green', 'black', 'yellow']
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  my_array = ['blue', 'green', 'black', 'yellow']
+  my_array << 'arrays!'
+  #puts my_array.inspect
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  my_array = ['blue', 'green', 'black', 'yellow']
+  my_array.unshift('wow')
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array = ["I", "am", "really", "learning", "arrays!"]
+  newArr = array.pop
+  newArr
 end
 
 def remove_element_from_start_of_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  newArr = array.shift
+  newArr
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  newArr = array[2]
+  newArr
 end
 
 def retrieve_first_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  newArr = array.first
+  newArr
 end
 
 def retrieve_last_element_from_array(array)
-  
+  array = ["wow", "I", "am", "really", "learning", "arrays!"]
+  newArr = array.last
+  newArr
 end
