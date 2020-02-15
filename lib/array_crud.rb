e7d76568d0c10ff3f8181ace99bb8718963f552d
Jen Miao <jenmiao@gmail.com>
2020-02-15 21:34:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,6 +20,7 @@ end
 def remove_element_from_end_of_array(array)
   array = ["I", "am", "really", "learning", "arrays!"]
   newArr = array.pop
+  newArr
 end
 
 def remove_element_from_start_of_array(array)
